a48d4e66ba91ab5ef3665a506680f2479f9aa3e1
Varsha <varsharaj575@gmail.com>
2024-09-02 21:38:25 +0530

    First Project File Added

---
+++ Project-1/main.tf
@@ -0,0 +1,129 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# creating an aws key-pair
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+# creating aws vpc
+resource "aws_vpc" "myvpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# creating a public subnet
+resource "aws_subnet" "mysubnet" {
+  vpc_id                  = aws_vpc.myvpc.id
+  cidr_block              = "10.0.0.0/24"
+  availability_zone       = "us-east-1"
+  map_public_ip_on_launch = true 
+}
+
+# creating an internet gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id                  = aws_vpc.myvpc.id
+}
+
+# creating a route table
+resource "aws_route_table" "rt" {
+  vpc_id                  = aws_vpc.myvpc.id
+  route = {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+# Associate the route table with the subnet.
+resource "aws_route_table_association" "rta" {
+  subnet_id      = aws_subnet.mysubnet.id
+  route_table_id = aws_route_table.rt.id
+}
+
+# creating a security group 
+resource "aws_security_group" "sg" {
+  name   = "web"
+  vpc_id = aws_vpc.myvpc.id
+
+  # ingress defines the kind of inbound-traffic allowed into the resources. such as http(port 80) and ssh(port 22)
+  ingress  {
+  description = "Http traffic"
+  from_port = 80
+  to_port = 80
+  protocol = "tcp"
+  cidr_blocks = "[0.0.0.0/0]"
+  }
+    
+  ingress {
+    description = "Allow HTTPS from anywhere"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+  description = "Allow SSh"
+  from_port = 22
+  to_port = 22
+  protocol = "tcp"
+  cidr_blocks = "[0.0.0.0/0]"
+  }
+
+  # egress defines the kind of outbound-traffic allowed to leave the resources. such as 
+  egress = {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+    tags = {
+    Name = "Web-sg"
+  }
+}
+
+# creating an ec2 instance
+resource "aws_instance" "server" {
+  ami                    = "ami-0261755bbcb8c4a84"
+  instance_type          = "t2.micro"
+  key_name = aws_key_pair.deployer.id
+  vpc_security_group_ids = [aws_security_group.sg.id]
+  subnet_id = aws_subnet.mysubnet.id
+
+  connection {
+    type = "ssh"
+    user = "ubuntu"
+    private_key = file("~/.ssh/id_rsa")
+    host = self.public_ip
+  }
+
+  # provisioners
+
+  provisioner "file" {
+    source = "app.py"
+    destination = "/home/ubuntu/app.py"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo 'Hello from the remote instance'",
+      "sudo apt update -y",  # Update package lists (for ubuntu)
+      "sudo apt-get install -y python3-pip", 
+      "cd /home/ubuntu",
+      "sudo pip3 install flask",
+      "sudo python3 app.py &",
+    ]
+  }
+}
